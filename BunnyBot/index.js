977e7244812c6973382275b7f72e54db25b7350d
FireTheSummoner <47149252+FireTheSummoner@users.noreply.github.com>
2023-02-14 02:34:21 -0500

    V1 Base
    base with no commands or connections of either bot

--- BunnyBot/index.js
+++ BunnyBot/index.js
@@ -1,1 +1,35 @@
 const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
+const { bunnyToken } = require('../tokens.json')
+
+class Client extends AkairoClient {
+    constructor() {
+        super(
+            {
+                ownerID: ['714973277457743983'],
+            },
+            {
+                intents: 131071,
+            },
+        );
+
+        //Change below when adding new folders or working with manual commands
+        /*this.commandHandler = new CommandHandler(this, {
+            directory: './main/commands/',
+            prefix: 'dk.',
+            automateCategories: true,
+        });*/
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './events/',
+        });
+        this.listenerHandler.setEmitters({
+            //commandHandler: this.commandHandler,
+            listenerHandler: this.listenerHandler,
+            process: process,
+        });
+        //this.commandHandler.useListenerHandler(this.listenerHandler);
+        //this.commandHandler.loadAll();
+        this.listenerHandler.loadAll();
+    }
+}
+const client = new Client();
+client.login(Token);
---
+++ index.js
@@ -0,0 +1,70 @@
+const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
+const { summonerToken, bunnyToken } = require('./tokens.json');
+
+class SummonerBot extends AkairoClient {
+    constructor() {
+        super(
+            {
+                ownerID: ['714973277457743983'],
+            },
+            {
+                disableMentions: 'everyone',
+                intents: 131071,
+            },
+        );
+
+        //Change below when adding new folders or working with manual commands
+        /*this.commandHandler = new CommandHandler(this, {
+            directory: './main/commands/',
+            prefix: 'dk.',
+            automateCategories: true,
+        });*/
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './SummonerBot/events/',
+        });
+        this.listenerHandler.setEmitters({
+            //commandHandler: this.commandHandler,
+            listenerHandler: this.listenerHandler,
+            process: process,
+        });
+        //this.commandHandler.useListenerHandler(this.listenerHandler);
+        //this.commandHandler.loadAll();
+        this.listenerHandler.loadAll();
+    }
+}
+
+class BunnyBot extends AkairoClient {
+    constructor() {
+        super(
+            {
+                ownerID: ['714973277457743983'],
+            },
+            {
+                intents: 131071,
+            },
+        );
+
+        //Change below when adding new folders or working with manual commands
+        /*this.commandHandler = new CommandHandler(this, {
+            directory: './main/commands/',
+            prefix: 'dk.',
+            automateCategories: true,
+        });*/
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './BunnyBot/events/',
+        });
+        this.listenerHandler.setEmitters({
+            //commandHandler: this.commandHandler,
+            listenerHandler: this.listenerHandler,
+            process: process,
+        });
+        //this.commandHandler.useListenerHandler(this.listenerHandler);
+        //this.commandHandler.loadAll();
+        this.listenerHandler.loadAll();
+    }
+}
+
+const summoner = new SummonerBot();
+const bunny = new BunnyBot();
+summoner.login(summonerToken);
+bunny.login(bunnyToken);
